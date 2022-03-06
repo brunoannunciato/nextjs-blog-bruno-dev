204335c4e01ec09fd0e8ca68ac4bcfefe566cdc8
brunoannunciato <brunobannunciato@gmail.com>
2022-03-06 17:29:31 -0300

    :sparkles: added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,22 @@
+import Script from 'next/script'
 import '../styles/globals.scss'
 import '../styles/prism-theme.scss'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Component {...pageProps} />
+
+    <Script strategy='lazyOnload' async src="https://www.googletagmanager.com/gtag/js?id=UA-120224377-2"/>
+    <Script strategy='lazyOnload'>
+      {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments)}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-120224377-2');
+      `}
+    </Script>
+  </>
 }
 
 export default MyApp
