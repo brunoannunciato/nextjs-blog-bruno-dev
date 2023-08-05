f86d0df8052e827fa09209a5cfcc3577bbdf8470
brunoannunciato <brunobannunciato@gmail.com>
2023-08-04 23:18:07 -0300

    chore: updated current year

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,22 +1,19 @@
-import Link from "next/link"
+import Link from "next/link";
 
-import styles from './footer.module.scss'
+import styles from "./footer.module.scss";
 
 const Footer = () => {
   return (
-
-    <footer className={ styles.footer }>
+    <footer className={styles.footer}>
       <p className="text">
-       Feito com <Link href="https://nextjs.org/" target="_blank">
-         <a>
-           Next.js
-         </a>
-       </Link> e ❤️ - 2022
-      
-       
+        Feito com{" "}
+        <Link href="https://nextjs.org/" target="_blank">
+          <a>Next.js</a>
+        </Link>{" "}
+        e ❤️ - 2023
       </p>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
